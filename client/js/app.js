7599ad9991a458266b6f64938e9d3f9f47e73148
BAMason <4emason@gmail.com>
2016-11-15 21:09:41 -0700

    part 2 of take one babel/webpack config

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,9 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+  </Router>,
+  document.getElementById('app-container')
+);
