caf527568579db2a3e16f70bd0874ab8eeb7d885
Matt Gardner <matthewlgardner@gmail.com>
2016-11-16 11:24:58 -0700

    add type form

--- modules/Entry.js
+++ modules/Entry.js
@@ -5,6 +5,16 @@ export default React.createClass({
     return (
       <div>
         <h2>{this.props.params.entry}</h2>
+        <div class="input-field col s12">
+          <select>
+            <option value="" disabled selected>Type</option>
+            <option value="New Moon">New Moon</option>
+            <option value="Full Moon">Full Moon</option>
+            <option value="Rest">Rest</option>
+            <option value="Practice">Practice</option>
+          </select>
+          <label>Type</label>
+        </div>
       </div>
     )
   }
