b31356c09d8553db4827e75bbcfdb55558ff00b4
Gordon H Graham <theboss@gordonhgraham.com>
2016-11-16 13:27:06 -0700

    class vs function in Photos component

--- modules/Photos.js
+++ modules/Photos.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import axios from 'axios'
 
+const postures = [`Bakasana`, `Malasana`, `Utanasana`, `Chaturunga Dandasana`].map((name, index) => {
+  return <option key={index}>{name}</option>
+})
+
 export default class Photos extends React.Component {
   constructor(props) {
     super(props)
 
     // const postures = axios()
-    const postures = [`Bakasana`, `Malasana`, `Utanasana`, `Chaturunga Dandasana`]
+    // const postures = [`Bakasana`, `Malasana`, `Utanasana`, `Chaturunga Dandasana`]
 
-    this.props = { postures: postures }
-
-    const renderPostures = (postures) => {
-      for (var i = 0; i < postures.length; i++) {
-        <option>postures[i]</option>
-      }
-    }
-
+    // this.props = { postures: postures }
 
     this.state = {
       posture: ''
     }
 
     this.handlePosture = this.handlePosture.bind(this)
+
+  // const RenderPostures = () => {
+  //
+  //   return postures.map((name, index) => {
+  //     return <option key={index}>{name}</option>
+  //   })
+  // }
+
   }
+
 
   handlePosture(e) {
     this.setState({ posture: e.target.value })
@@ -31,8 +37,9 @@ export default class Photos extends React.Component {
   render() {
     return (
       <div>
-        <select value={this.state.posture} onChange={this.handlePosture.bind(this)}/>
-        {renderPostures(postures)}
+        <select value={this.state.posture} onChange={this.handlePosture.bind(this)}>
+          {postures}
+        </select>
       </div>
     )
   }
